b773513df7beb879b9fd2b3ee3b6144392ba7e4a
Michael <WTMike24@users.noreply.github.com>
2017-10-19 15:31:51 -0400

    Create oauth.py

---
+++ BL2Keys/oauth.py
@@ -0,0 +1,15 @@
+# To get your authentication keys:
+#   1) Go to apps.twitter.com
+#   2) Create an app
+#   3) Go to the Keys and Access Tokens tab
+#   4) "Consumer Key (API Key)" is your consumer_key
+#      "Consumer Secret (API Secret)" is your consumer_secret
+#   5) Click "Create my access token" at the bottom of the page
+#   6) "Access Token" at the bottom of the page is now your access_token
+#      "Access Token Secret" at the bottom is now your access_secret
+#   7) If desired, change permissions to read only, this bot sends no tweets
+
+consumer_key = 'YOUR KEY HERE'
+consumer_secret = 'YOUR KEY HERE'
+access_token = 'YOUR KEY HERE'
+access_secret = 'YOUR KEY HERE'
