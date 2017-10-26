8e09bbbfd334a2d5fdc9b326c214c7db9e38713d
Michael <WTMike24@users.noreply.github.com>
2017-10-26 18:55:37 -0400

    Huge update to tester.py
    Basically finished as a shell script. Just needs some polishing

--- BL2Keys/tester.py
+++ BL2Keys/tester.py
@@ -2,37 +2,20 @@ import tweepy, re, datetime, os, sys
 from tweepy import OAuthHandler
 from time import sleep
 from oauth import *
+from constants import *
 
 #Secret keys
 #Keys are now stored in oauth.py
 
 #Kinda want this to also be a standalone version, so if you're on linux, you get colors!
-if (os.name == 'nt'):
-	none = ''
-	bold = ''
-	underline = ''
-
-	red = ''
-	green = ''
-	yellow = ''
-	blue = ''
-	purple = ''
-	cyan = ''
-	white = ''
-	reset = ''
+if (os.name == 'nt' or '--nocolor' in sys.argv):
+	none, bold, underline = '', '', ''
+	red, yellow, green, cyan, blue, purple, white, reset = '', '', '', '', '', '', '', ''
 else:
-	none = '\x1b[0;'
-	bold = '\x1b[1;'
-	underline = '\x1b[4;'
-
-	red = '31;40m'
-	green = '32;40m'
-	yellow = '33;40m'
-	blue = '34;40m'
-	purple = '35;40m'
-	cyan = '36;40m'
-	white = '37;40m'
-	reset = '\x1b[0m'
+	none, bold, underline = '\x1b[0;', '\x1b[1;', '\x1b[4;'
+	red, green, yellow, blue, purple, cyan, white, reset = '31m', '32m', '33m', '34m', '35m', '36m', '37m', '\x1b[0m'
+	#Hijack the OS check to disable the cursor on linux systems
+	os.system('setterm -cursor off')
 
 #Create regex stuff
 dateRegex = re.compile("\d{4}-(\d{2})-(\d{2}) \d{2}:\d{2}:\d{2}\.\d+")
@@ -41,13 +24,138 @@ expireRegex = re.compile("Active through (\d+)\/(\d+)")
 
 #Global variables
 keys = []
-expire = []
-mode = 0
+expires = []
 today = dateRegex.search(str(datetime.datetime.now())).groups()
+tweets = []
+showAll = 0
+printLine = 0
+printData = ''
+
+#Function calls because they have to go before the executed code :(
+def getTweets():
+	global tweets
+	#If we request anything above 200, we only get 200, so why bother trying
+	raw_tweets = api.user_timeline(screen_name = 'Borderlands', count=200)
+	#2D array taken from https://gist.github.com/yanofsky/5436496
+	tweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in raw_tweets]
+
+def extractTweets(keyTweet, expireyTweet):
+	global keys
+	global expires
+	#Attempt to extract the key from the tweet
+	keyResults = keyRegex.search(str(keyTweet))
+	#Check to see if we can extract the keys from the tweet
+	#keyResults == None if we can't
+	if keyResults:
+		#We got it! make groups so we can extract the keys
+		keyResults = keyResults.groups()
+		list = []
+		for c in range(len(keyResults)):
+			#Because of my regex string, only the even ones are the full keys
+			if c % 2 == 0:
+				list.append(str(keyResults[c]));
+		keys.append(list)
+		#Now we gotta get the expiration date
+		expireResults = expireRegex.search(str(expireyTweet))
+		#Make sure the regex worked (it should)
+		if expireResults:
+			expireResults = expireResults.groups()
+			expires.append(expireResults)
+		else:
+			print(bold+red+"Something broke with the expiration regex, pls fix"+reset)
+			print(none+red+"regex: " + str(expireRegex)+reset)
+			print(none+red+"tweet: " + str(tweets[index+1][2])+reset)
+	#regex broke, what's our data
+	else:
+		print(bold+red+"Something broke with the key regex, pls fix"+reset)
+		print(none+red+"regex: " + str(keyRegex)+reset)
+		print(none+red+"tweet: " + str(tweet[2])+reset)
+
+#This function will handle neatening up the output
+def printer(data, working):
+	#Gotta use global variables or we won't actually get anything done
+	global printData
+	global keyMode
+	barColor = none+blue
+	headerColor = bold+blue
+	#Set up the top 3 lines of the table
+	printData += reset
+	if keyMode == 1 or keyMode == 2 or keyMode == 4:
+		printData += barColor + barColor + str(bars['top'][0]) + '\n' + reset
+		printData += barColor + str(bars['left']) + reset + headerColor + (str(headers['pc']) if keyMode == 1 else str(headers['xbone']) if keyMode == 2 else str(headers['ps4'])) + barColor + str(bars['right']) + '\n' + reset
+		printData += barColor + str(bars['center'][0]) + '\n' + reset
+	elif keyMode == 3 or keyMode == 5 or keyMode == 6:
+		printData += barColor + str(bars['top'][1]) + '\n' + reset
+		if keyMode == 3:
+			printData += barColor + str(bars['left']) + str(headers['pc']) + barColor + str(bars['middle']) + str(headers['xbone']) + barColor + str(bars['right']) + '\n' + reset
+		if keyMode == 5:
+			printData += barColor + str(bars['left']) + str(headers['pc']) + barColor + str(bars['middle']) + str(headers['ps4']) + barColor + str(bars['right']) + '\n' + reset
+		if keyMode == 6:
+			printData += barColor + str(bars['left']) + str(headers['xbone']) + barColor + str(bars['middle']) + str(headers['ps4']) + barColor + str(bars['right']) + '\n' + reset
+		printData += barColor + str(bars['center'][1]) + '\n' + reset
+	elif keyMode == 7:
+		printData += barColor + str(bars['top'][2]) + '\n' + reset
+		printData += barColor + str(bars['left']) + str(headers['pc']) + barColor + str(bars['middle']) + str(headers['xbone']) + barColor + str(bars['middle']) + str(headers['ps4']) + barColor + str(bars['right']) + '\n' + reset
+		printData += barColor + str(bars['center'][2]) + '\n' + reset
+	printData += reset
+
+	#Loop through the keys we got and display them
+	for c in range(len(data)):
+		if not showAll and not working[c]:
+			continue
+		else:
+			if keyMode == 1 or keyMode == 2 or keyMode == 4:
+				printData += reset + barColor + str(bars['left']) + printerColor(working[c]) + (str(data[c][0]) if keyMode == 1 else str(data[c][1]) if keyMode == 2 else str(data[c][2])) + reset + barColor + str(bars['right']) + '\n' + reset
+			elif keyMode == 3:
+				printData += reset + barColor + str(bars['left']) + printerColor(working[c]) + str(data[c][0]) + reset + barColor + str(bars['middle']) + printerColor(working[c]) + str(data[c][1]) + reset + barColor + str(bars['right']) + '\n' + reset
+			elif keyMode == 5:
+				printData += reset + barColor + str(bars['left']) + printerColor(working[c]) + str(data[c][0]) + reset + barColor + str(bars['middle']) + printerColor(working[c]) + str(data[c][2]) + reset + barColor + str(bars['right']) + '\n' + reset
+			elif keyMode == 6:
+				printData += reset + barColor + str(bars['left']) + printerColor(working[c]) + str(data[c][1]) + reset + barColor + str(bars['middle']) + printerColor(working[c]) + str(data[c][2]) + reset + barColor + str(bars['right']) + '\n' + reset
+			elif keyMode == 7:
+				printData += reset + barColor + str(bars['left']) + printerColor(working[c]) + str(data[c][0]) + reset + barColor + str(bars['middle']) + printerColor(working[c]) + str(data[c][1]) + reset + barColor + str(bars['middle']) + printerColor(working[c]) + str(data[c][2]) + reset + barColor + str(bars['right']) + '\n' + reset
+
+	#Stick the bottom of the table on
+	printData += reset + barColor + (str(bars['bottom'][0]) if (keyMode == 1 or keyMode == 2 or keyMode == 4) else str(bars['bottom'][1]) if (keyMode == 3 or keyMode == 5 or keyMode == 6) else str(bars['bottom'][2])) + '\n' + reset
+
+	print(printData)
+
+def printerColor(data):
+	return none + (yellow if data and data == 2 else green if data else red)
+
+def debug(data):
+	print(bold+purple+str(data)+reset, end='')
+#No more functions :D
+
+#Everything needs help text!
+if "--help" in sys.argv:
+	helptext = ''
+	helptext += '\n'+bold+yellow+'Welcome to a key reader for Borderlands 2 Golden Crate Keys!'
+	helptext += '\nThis program searches the Borderlands twitter and pulls down'
+	helptext += '\nGolden Crate keys for Borderlands 2.'+reset+none+yellow
+	helptext += '\n\nThere are a few options you can use to change how this runs:'
+	helptext += '\n\n--help\tDisplays this help message and exits'
+	helptext += '\n--all\tDisplays all keys, expired or not, and says whether they are active'
+	helptext += '\n\n--pc\tDisplays keys only for PC   \\'
+	helptext += '\n--ps4\tDisplays keys only for PS4   }Can be combined to show multiple consoles'
+	helptext += '\n--xbox\tDisplays keys only for XBox /'
+	helptext += '\n\nThe default settings are as follows:'
+	helptext += '\n\nDisplay only active keys\nDisplay only PC keys'
+
+	print(none+yellow+helptext+reset)
+	sys.exit()
 
 #Do we display all or just the valid ones? Valid only is default
-if ("--all" in sys.argv or '-a' in sys.argv):
-	mode = 1
+if ('--all' in sys.argv or '-a' in sys.argv):
+	showAll = 1
+#We're gonna use numbers like unix rwx to check pc/xbone/ps, PC is default
+keyMode = 1
+if ('--pc' not in sys.argv and ('--ps4' in sys.argv or '--xbox' in sys.argv)):
+	keyMode = 0
+if '--ps4' in sys.argv:
+	keyMode += 4
+if '--xbox' in sys.argv:
+	keyMode += 2
 
 #Create our authentication
 auth = OAuthHandler(consumer_key, consumer_secret)
@@ -55,57 +163,49 @@ auth.set_access_token(access_token,access_secret)
 api = tweepy.API(auth)
 
 #Begin
-print(none+green+"BEGIN\n"+reset)
-raw_tweets = api.user_timeline(screen_name = 'Borderlands', count=250)
-
-#2D array taken from https://gist.github.com/yanofsky/5436496
-tweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in raw_tweets]
+debug("BEGIN\n\n")
+getTweets()
 
 #Go through the tweets and print the code tweets and the expirey tweets for BL2
 index = 0
 for tweet in tweets:
-	sleep(0.01)
 	if "BL2" in str(tweet[2]):
+		#If index+1 is equal to the length of the array, we're done. An index at the length is 1 beyond the last index
+		if index+1 == len(tweets):
+			#Break out of the for loop, there's no point in going further
+			break
 		if "Unlock" not in str(tweets[index+1][2]):
-			#This is if the expiration code tweet isn't where we expect it to be, TODO: search for tweet within 2 tweets on either side, if not found, expirey unknown
-			# print("------------------ERROR------------------")
-			# for c in range(9):
-			# 	q = c-4
-			# 	print("# " + str(index+q) + " -- " + str(tweets[index+q][2]))
-			# print("-------------------END-------------------")
-			continue
+			#This is if the expiration code tweet isn't where we expect it to be
+			#Search 9 tweets (4 under, 4 above, code is the 1 in the center)
+			for c in range(9):
+				q = c-4
+				if "Unlock" in str(tweets[index-q][2]) and "Borderlands 2" in str(tweets[index-q][2]):
+					extractTweets(tweet[2], tweets[index-q][2])
+					break
 		else:
-			#Attempt to extract the key from the tweet
-			keyResults = keyRegex.search(str(tweet[2]))
-			#Check to see if we can extract the keys from the tweet
-			#keyResults == None if we can't
-			if keyResults:
-				#We got it! make groups so we can extract the keys
-				keyResults = keyResults.groups()
-				list = []
-				for c in range(len(keyResults)):
-					#Because of my regex string, only the even ones are the full keys
-					if c % 2 == 0:
-						list.append(str(keyResults[c]));
-				keys.append(list)
-				#Now we gotta get the expiration date
-				expireResults = expireRegex.search(str(tweets[index+1][2]))
-				#Make sure the regex worked (it should)
-				if expireResults:
-					expireResults = expireResults.groups()
-
-				else:
-					print(bold+red+"Something broke with the expiration regex, pls help"+reset)
-					print(none+red+"regex: " + str(expireRegex)+reset)
-					print(none+red+"tweet: " + str(tweets[index+1][2])+reset)
-			#regex broke, what's our data
-			else:
-				print(bold+red+"Something broke with the key regex, pls help"+reset)
-				print(none+red+"regex: " + str(keyRegex)+reset)
-				print(none+red+"tweet: " + str(tweet[2])+reset)
+			#Pass the tweets required to a function that parses and stores the data we need
+			extractTweets(tweet[2], tweets[index+1][2])
 	index += 1
+	debug(str(index)+"\r")
+	sleep(0.01)
+#Go through and display them, red for dead (only show if --all), yellow for dieing today, green for good
+working = []
+for c in range(len(keys)):
+	expire = expires[c]
+	if ((today[0] > expire[0] and today[1] > expire[1]) or (today[0] > expire[0])):
+		working.append(0)
+	elif (today[0] == expire[0] and today[1] > expire[1]):
+		working.append(0)
+	elif (today[0] == expire[0] and today[1] == expire[1]):
+		working.append(2)
+	else:
+		working.append(1)
+printer(keys, working)
+
 #Don't print a newline at the end for windows, print a newline for linux
 if 'nt' not in os.name:
-	print(none+green+'\nEND'+reset)
+	debug('\nEND\n')
+	#Enable the cursor again so we don't get in trouble
+	os.system('setterm -cursor on')
 else:
-	print(none+green+'\nEND'+reset, end='')
+	debug('\nEND')
